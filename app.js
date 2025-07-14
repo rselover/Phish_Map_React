b13db1551288b2cba8e55eb6cde8549efd5ec32d
Rob Selover <rselover@users.noreply.github.com>
2025-07-14 12:36:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+import './App.css';
+
 const e = React.createElement;
 
 function App() {
-  return e('div', null,
+  return e('div', { className: 'App' },
     e('h1', null, 'PHISH SONG COUNT BY CITY'),
     e('p', null, 'Find Yourself a City to Live In – Songs from every Phish show aggregated by City'),
     e('div', { id: 'map', style: { width: '100vw', height: '90vh' } })
