30f822df05e09ad1746f2e94a4cd51b3d2d9f50e
Rob Selover <rselover@users.noreply.github.com>
2025-07-14 11:26:07 -0400

    Create map.js

---
+++ map.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import * as d3 from 'd3';
+import mapboxgl from 'mapbox-gl';
+import { HexagonLayer, MapboxOverlay } from 'deck.gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicnNlbG92ZXIiLCJhIjoiY21hbGJxMGxjMDZ6MDJtb3JqMWx5ZHh0YSJ9.50NQFxaZLsWruJ8dFIqfXw';
+
+const csvUrl = 'https://gist.githubusercontent.com/rselover/9d4c1543a8dc994ca151cff20aa8fe1f/raw/5a99753828073ee1d90d1b2bbf4da2393209e414/phish_geocode.csv';
+
+const Map = () => {
+  useEffect(() => {
+    d3.csv(csvUrl, row => ({
+      position: [+row.lon, +row.lat]
+    })).then(data => {
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/dark-v11',
+        center: [-95, 40],
+        zoom: 3,
+        pitch: 20,
+        bearing: 0
+      });
+
+      map.on('style.load', () => {
+        map.setProjection('mercator');
+      });
+
+      map.on('load', () => {
+        const hexLayer = new HexagonLayer({
+          id: 'phish-hex-layer',
+          data,
+          radius: 20000,
+          elevationScale: 250,
+          extruded: true,
+          pickable: true,
+          getPosition: d => d.position,
+          colorRange: [
+            [255, 255, 204],
+            [255, 204, 204],
+            [255, 153, 153],
+            [255, 102, 102],
+            [255, 51, 51],
+            [255, 0, 0]
+          ],
+          opacity: 0.9
+        });
+
+        const deckOverlay = new MapboxOverlay({ layers: [hexLayer] });
+        map.addControl(deckOverlay);
+      });
+    });
+  }, []);
+
+  return <div id="map" style={{ width: '100%', height: '90vh' }} />;
+};
+
+export default Map;
