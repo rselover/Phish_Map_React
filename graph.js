fef563e5b2d73dd11a5a2dd477129731ad2e41f3
Rob Selover <rselover@gmail.com>
2025-07-21 11:16:01 -0400

    graph csv url

--- graph.js
+++ graph.js
@@ -1,4 +1,4 @@
-const csvUrl = 'https://gist.githubusercontent.com/rselover/9d4c1543a8dc994ca151cff20aa8fe1f/raw/5a99753828073ee1d90d1b2bbf4da2393209e414/phish_geocode.csv';
+const graphCsvUrl = 'https://gist.githubusercontent.com/rselover/9d4c1543a8dc994ca151cff20aa8fe1f/raw/5a99753828073ee1d90d1b2bbf4da2393209e414/phish_geocode.csv';
 
 function renderShowsByYearPlot() {
   loadGraphhData().then(data => {
